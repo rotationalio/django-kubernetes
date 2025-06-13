815f2fd395edaf4f979f22291d37dfc7966c3cfd
Benjamin Bengfort <benjamin@bengfort.com>
2025-06-13 15:16:36 -0500

    Django App Setup

---
+++ djk8s/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class Djk8SConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'djk8s'
+    label = 'kubernetes'
+    verbose_name = 'Django Kubernetes'
